aefe9dae07985879046adebab0d232693f86f74a
李戈 <ligeislogic@gmail.com>
2016-05-12 01:17:24 +0800

    修改增加ip的问题

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -90,8 +90,8 @@ class OrdersController < ApplicationController
       payment.amount = 500
       payment.channel = 'alipay_qr'
       payment.currency = "cny"
-      payment.client_ip = "127.0.0.1"
-     # payment.client_ip = request.remote_ip
+     # payment.client_ip = "127.0.0.1"
+      payment.client_ip = request.remote_ip
 
       payment.save
 
