ce994f71f23c0e2c745318239d41bdd189b98a84
李戈 <ligeislogic@gmail.com>
2016-05-15 18:55:16 +0800

    修改调试的断点

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,6 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Admin::AdminsController < Admin::ApplicationController
-  layout "admin"
   before_action :set_admin_user, only: [:show, :edit,:shanchu, :update, :destroy]
 
   def index
